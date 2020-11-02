b8068448a3409062e7c4ec2f95e7e10b1149f635
shubhampatwa30 <39874851+shubhampatwa30@users.noreply.github.com>
2020-11-01 19:44:25 -0500

    insert done

--- controllers/routing.js
+++ controllers/routing.js
@@ -7,26 +7,55 @@ const projectModel =  mongoose.model("project");
 const searchController = require("./search");
 const updateController = require("./update");
 const removeController = require("./remove");
+
 router.get("/",(req,res)=>{
 
     res.send("Router");
 })
 
-router.get("/add",(req,res)=>{
+router.get("/create",(req,res)=>{
 
-    res.render("addCourse");
+    res.render("create");
 })
 
-router.post("/add",(req,res)=>{
-    
+router.post("/create",(req,res)=>{
+    var length =1;
+    projectModel.find({ "id": req.body.id},(err, docs)=>{ 
+        if(!err){
+            
+           // console.log(docs);
+         length = (docs.length);
+        }
+        else{
+            res.send(err);
+        }
+     }
+    )
+    if(length == 0){
+
     var project = new projectModel();
-    project.id = req.body.id;
-    project.name = req.body.name;
-    project.date = req.body.date;
-    project.manner_of_death = req.body.manner_of_death;
+    
+    project.id  = req.body.id;
+project.name = req.body.name;
+project.date = req.body.date;
+project.manner_of_death = req.body.manner_of_death;
+project.armed = req.body.armed_type;
+project.age = req.body.age;
+project.gender= req.body.gender;
+project.race = req.body.race;
+project.city = req.body.city;
+project.state = req.body.state;
+project.signs_of_mental_illness = req.body.signs_of_mental_illness;
+project.threat_level = req.body.threat_level;
+project.flee = req.body.flee;
+project.body_camera = req.body.body_camera;
+project.arms_category = req.body.arms_category;
+
+
     project.save((err,docs) => {
             if(!err){
-                res.redirect("/routing/list");
+                res.send("Insert Successful");
+                
             }
             else{
                 res.send(err);
@@ -34,12 +63,18 @@ router.post("/add",(req,res)=>{
     });
   
     
+    }
+    else{
+        res.send("Id already exists");
+    }
+
+
 });
 
 
 
-router.get("/list",async(req,res)=>{
- await projectModel.find((err, docs)=>{ 
+router.get("/list",(req,res)=>{
+  projectModel.find((err, docs)=>{ 
     if(!err){
         res.render("list",{data :docs});
         console.log(docs);
@@ -52,5 +87,6 @@ router.get("/list",async(req,res)=>{
 router.use("/search",searchController);
 router.use("/update",updateController);
 router.use("/remove",removeController);
+
 
 module.exports = router;
