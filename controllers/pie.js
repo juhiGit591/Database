5168ea9a1e0eae335a188a187c17271b5569bcdf
shubhampatwa30 <39874851+shubhampatwa30@users.noreply.github.com>
2020-11-19 18:31:37 -0500

    update

--- controllers/pie.js
+++ controllers/pie.js
@@ -11,18 +11,30 @@ pie.get("/",(req,res)=>{
 
 pie.post("/",(req,res)=>{
    
-    
-    
             pieSchema.aggregate([
                 {
                     $match : { state : req.body.state}
                 },
                 { $group: { _id: "$race", count: { $sum: 1 } } }
              ],(err,docs)=>{
+                 var total = 0;
+                 for(var i = 0;i<docs.length;i++){
+                     total += docs[i].count;
+                     
+                 }
+                 for(var i = 0;i<docs.length;i++){
+                    docs[i].count = docs[i].count*100/total;
+                   
+                    
+                }
+
+
                  res.render("displayAnalysis",{data : docs});   
              }
             ); 
 
+
+           
     
     }
     );
